2ec9c2fbc9c6f110e013fc2bcae7586a6d354d80
Luigi Paschoalino <64054620+luigi-paschoalino@users.noreply.github.com>
2024-02-06 21:46:19 -0300

    chore: event model no prisma

--- src/core/infra/models/Event.model.ts
+++ src/core/infra/models/Event.model.ts
@@ -1,13 +1,13 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+// import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
-@Entity({ name: 'event-log' })
-export class EventModel extends BaseEntity {
-    @PrimaryGeneratedColumn('uuid')
-    id: string
+// @Entity({ name: 'event-log' })
+// export class EventModel extends BaseEntity {
+//     @PrimaryGeneratedColumn('uuid')
+//     id: string
 
-    @Column()
-    eventName: string
+//     @Column()
+//     eventName: string
 
-    @Column({ type: 'jsonb' })
-    eventData: unknown
-}
+//     @Column({ type: 'jsonb' })
+//     eventData: unknown
+// }
